b904aadf287e04e3955d6f9bfa296c379eaee37f
Matteociurletti <128637698+Ciurlaz@users.noreply.github.com>
2025-06-03 08:46:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
+from flask import Flask, render_template
+import sqlite3
+import os
+
+app = Flask(__name__)
+DB_PATH = os.path.join("data", "bakery.db")
+
+def get_prodotti():
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("SELECT nome, descrizione, prezzo, immagine FROM prodotti")
+    prodotti = cur.fetchall()
+    conn.close()
+    return prodotti
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/prodotti")
+def prodotti():
+    items = get_prodotti()
+    return render_template("prodotti.html", prodotti=items)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
